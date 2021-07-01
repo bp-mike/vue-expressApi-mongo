7fe969ccf078dc1f87c4cd0ba63d9160426433bb
bp mike <bpmikeomo@gmail.com>
2021-07-01 14:49:15 +0300

    adding static folder

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ app.use(cors())
 
 const posts = require('./routes/api/posts')
 app.use('/api/posts', posts);
+// __ handle production 
+if(process.env.NODE_ENV === 'production'){
+    // ___ static folder
+    app.use(express.static(__dirname + '/public/'))
+    //___ handle SPA
+    app.get(/.*/, (req, res) =>{
+        res.sendFile(__dirname + '/public/index.html');
+    })
+}
 
 const port = process.env.port || 5000
 
